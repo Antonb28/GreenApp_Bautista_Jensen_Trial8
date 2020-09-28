7bf42f8d137fe66c9dfb77cd3bfdc90acac07ea0
Antonb28 <62889116+Antonb28@users.noreply.github.com>
2020-09-28 16:25:43 +0800

    PrintingAdded

--- src/main/java/org/example/Printing.java
+++ src/main/java/org/example/Printing.java
@@ -1,12 +1,23 @@
 package org.example;
 
 import javafx.fxml.FXML;
-
+import javafx.stage.FileChooser;
+import javafx.scene.control.*;
 import java.io.IOException;
 
 public class Printing {
     @FXML
+    Button SelectFile;
+
+    @FXML
     private void Back() throws IOException {
         App.setRoot("MainMenu");
+    }
+
+    @FXML
+    private void fileSelect() throws IOException{
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File");
+        fileChooser.showOpenDialog(null);
     }
 }
